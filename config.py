2cec7377132f6e335610614aee56e00adc1b746b
Anshu-m-dev <priyanshu.m.dev@gmail.com>
2026-03-03 16:23:57 +0530

    test

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
+        host="db",
         user="flaskuser",
         password="password123",
         database="incident_db"
